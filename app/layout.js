5bd29d44ccdf91ba59ae01b48f74852920d9ce49
SankThomas <thomassankara74@gmail.com>
2025-04-16 19:18:46 +0300

    add themeprovider, toaster, navbar

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/navbar";
+import { Toaster } from "sonner";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,11 +23,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Toaster />
+          <Navbar />
+          <div className="px-4 py-32">{children}</div>
+        </ThemeProvider>
       </body>
     </html>
   );
